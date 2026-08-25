b9f5adfc2641b1c75945c9f57c8a0c1b586f3cfb
Wilfried White <Wilfriedwhite777@gmail.com>
2026-08-25 11:31:54 +0100

    nice

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,7 +19,7 @@ class DatabaseSeeder extends Seeder
             DigitalCourseSeeder::class,
             ProfilePackSeeder::class,
             RoadmapSeeder::class,
-            
+            BibliothequeSeeder::class,
         ]);
     }
 }
